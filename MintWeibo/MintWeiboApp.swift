ccfd468d758314d8b8e22d92db254e55c993b609
M1nt666 <87382339+M1nt666@users.noreply.github.com>
2022-07-19 22:47:11 +0800

    Initial Commit

---
+++ MintWeibo/MintWeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintWeiboApp.swift
+//  MintWeibo
+//
+//  Created by Mint on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct MintWeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
